9eab5c9900424dd2d04577523b5188cfca87397b
kennedybabu <kennedynjogu8@gmail.com>
2021-12-04 09:16:41 +0300

    feature: create the profile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    bio = models.TextField(blank=True)
+    followers = ManyToManyField(User, related_name='followers')
