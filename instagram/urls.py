68485f1265be7b07f8252f2e886e648581e9c662
kennedybabu <kennedynjogu8@gmail.com>
2021-12-08 10:45:11 +0300

    update: change urls from path to url

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from django.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('app.urls')),
 ]
