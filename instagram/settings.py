f4c2fbff1bbc16e618046ed2abf304654780710e
kennedybabu <kennedynjogu8@gmail.com>
2021-12-08 10:19:59 +0300

    changes before deployment to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
     'app.apps.AppConfig',
     'bootstrap3',
     'django.contrib.admin',
@@ -87,6 +92,12 @@ DATABASES = {
         'PASSWORD': 'new',
     }
 }
+
+cloudinary.config(
+  cloud_name = config('CLOUDINARY_NAME'),
+  api_key = config('CLOUDINARY_API_KEY'),
+  api_secret = config('CLOUDINARY_SECRET')
+)
 
 
 # Password validation
